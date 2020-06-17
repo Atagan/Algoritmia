260f2d5c49d14ec8b745f5aed8541b6c64ff373e
Laura Ramos <luis@Luiss-MacBook-Air.local>
2020-06-17 18:37:18 +0200

    cosos de una serpiente

--- Entegas/EntregaTeoria/ejercicio4.py
+++ Entegas/EntregaTeoria/ejercicio4.py
@@ -50,10 +50,11 @@ def init(datos):
     se encuentran dentro del tablero
 """
 def valido (x,y, mapa):
-    filas = len(mapa)
-    columnas = len(mapa[1])
+    filas = len(mapa)-1
+    columnas = len(mapa[1])-1
+    print(x,y)
     if (x>=0 and x<=filas and y>=0 and y<=columnas):
-        if mapa[x][y] != 'X' and mapa[x][y] == 0:
+        if mapa[x][y] != 'X' and mapa[x][y] == '0':
             return True
         else:
             return False
@@ -70,13 +71,14 @@ def valido (x,y, mapa):
         2 -> abajo
 
 """
-def camion (x,y, numMov, mapa, direccion, numDirDer):
-    mapa[x][y] = numMov
-    
-    if mapa[x][y] == 2:
+def camion (x,y, numMov, mapa, direccion, numDirDer):    
+    if mapa[x][y] == 'Z':
+        print("True")
+        mostrarMatriz(0,0,4,4, mapa)
         return True, mapa, numDirDer
 
     else:
+        mapa[x][y] = numMov
         if direccion == 8:
             nuevoX = x-1
             nuevoY = y
@@ -88,7 +90,7 @@ def camion (x,y, numMov, mapa, direccion, numDirDer):
         elif direccion == 6:
             nuevoX = x
             nuevoY = y+1
-            nuevoX1 = x-1
+            nuevoX1 = x+1
             nuevoY1 = y
             nuevaDireccion = 6
             nuevoDireccion1 = 2
@@ -113,35 +115,52 @@ def camion (x,y, numMov, mapa, direccion, numDirDer):
             print("Pues la has cagao")
             
         if valido(nuevoX,nuevoY,mapa):
+            print("voy recto, mov: ", numMov)
+            mostrarMatriz(0,0,4,4, mapa)
             if camion(nuevoX, nuevoY, numMov+1, mapa, nuevaDireccion, numDirDer):
                 return True, mapa, numDirDer
             else:
-                return False
+                #mapa[x][y] = '0'
+                mostrarMatriz(0,0,4,4, mapa)
             
         if valido(nuevoX1, nuevoY1, mapa):
+             print("giro, mov: ", numMov)
+             mostrarMatriz(0,0,4,4, mapa)
              if camion(nuevoX, nuevoY, numMov+1, mapa, nuevoDireccion1, numDirDer+1):
                 return True, mapa, numDirDer
              else:
-                return False
+                #mapa[x][y] = '0'
+                mostrarMatriz(0,0,4,4, mapa)
             
-        mapa[x][y] = 0
-        return False
+    mapa[x][y] = '0'
+    mostrarMatriz(0,0,4,4, mapa)
+    return False
     
 def encontrar(mapa):
     for i in range(len(mapa)):
         for j in range(len(mapa[1])):
             if mapa[i][j]=='1':
                 return i,j
-            
+
+def mostrarMatriz(filaC, columC, filaF, columF, matriz):
+    for fila in range(filaC, filaF):
+        for columna in range(columC, columF):
+            print(str(matriz[fila][columna]) + " ", end='')
+        print("\n")
+    print("\n")
+
+
+
 def main():
      d = leerData("test.txt")
      # print(d)
      filas, columnas, mapa = init(d)
-     print(mapa)
+
      x,y = encontrar(mapa)
-     print(x,y)
+
      
-     print(camion(x,y,0,mapa,6,0))
-     
+     #isrutita,rutita, numrutita=camion(x,y,1,mapa,4,0)
+     camion(x,y,1,mapa,4,0)
+
     
 main()
